6a604dd3d2403f02101a60761c18e0afaccd3859
eric.roys@gmail.com <eric.roys@gmail.com>
2024-08-05 08:47:17 -0500

    feature: seeding
    added preliminary test data and logic to import it

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,13 @@
-import { dbConnect } from "../config/connection.js";
-import { User, Blog, Blog_Comment } from "../models/index.js"
+import { dbConnect } from '../config/connection.js';
+import { User, Blog, Blog_Comment } from '../models/index.js';
+import { readFile } from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-await dbConnect.sync({force: true})
+await dbConnect.sync({ force: true });
+
+//import test data
+User.bulkCreate(JSON.parse(await readFile(__dirname + '/data/user.json')));
+Blog.bulkCreate(JSON.parse(await readFile(__dirname + '/data/blog.json')));
+Blog_Comment.bulkCreate(JSON.parse(await readFile(__dirname + '/data/comments.json')));
